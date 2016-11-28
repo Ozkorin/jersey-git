8a19ecc9c24a203df269e1d73512b3e89578ed8c
Ozkorin <hg2t4e@gmail.com>
2016-11-28 19:44:40 +0100

    Updated Endpoint.java

--- src/main/java/jerseyRestApiForSimutool/Endpoint.java
+++ src/main/java/jerseyRestApiForSimutool/Endpoint.java
@@ -18,35 +18,44 @@ public class Endpoint {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String responseToGetRequest() {
-		return "Get Request Reply Comes Here1"; 
+		return "Get Request Reply Comes Here 2";  
 		// return Response.status(201).type(MediaType.TEXT_PLAIN).build();
 	}
 
 	@POST
 	@Path("activity")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String responseToPostRequest(final String incomingActivityJson) {
+	public Response responseToPostRequest(final String incomingActivityJson) {
 
 		// GSON is used to convert JSON to POJO
 		Gson jsonToPojo = new Gson();
 		//Error Checking
 		if (jsonToPojo == null)
-			return "Gson Object jsonToPojo is null";
+			return Response.status(400).build();
+			//String Return Below
+			//return "Gson Object jsonToPojo is null";
 		
 		// The following code does the conversion.
 		Activity activityPojoFromJson = jsonToPojo.fromJson(incomingActivityJson, Activity.class);
 		//Error Checking
 		if (activityPojoFromJson == null)
-			return "Activity Object activityPojoFromJson is null";
+			return Response.status(400).build();
+			//String Return Below
+			//return "Activity Object activityPojoFromJson is null";
 		
 		ST st = new ST(activityPojoFromJson.getUploader(), activityPojoFromJson.getPurpose(), 
 				activityPojoFromJson.getDescription(), activityPojoFromJson.getAttachment(), 
 				activityPojoFromJson.getVisibility(), activityPojoFromJson.getUpload_date(),
 				activityPojoFromJson.getTags());
 		
-		return st.getLinearTag();
+		//Code To check if linearization of tags using string builder is working or not
+		//return st.getLinearTag();
 
+		//Code to see if activityPojoFromNull has values or not
 		//return activityPojoFromJson.getPurpose();
+		
+		//Return ok
+		return Response.status(200).build();
 	}
 
 }
